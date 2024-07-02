ff8bd0a272460ff7f16902925f70538f020ed453
xujunsheng <1725116377@qq.com>
2024-07-02 14:38:13 +0800

    chore:安装pinia

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
-	<div class="appTest flex">
-		<div class="child">App1</div>
-		<div class="child">App2</div>
-		<el-button type="primary">App</el-button>
-	</div>
+  <div class="flex appTest">
+    <div class="child">App1</div>
+    <div class="child">App2</div>
+    <el-button type="primary" @click="handleIncrease">{{ currrentCount }}</el-button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useCounterStore } from '@/stores/modules/counter'
+
+const couter = useCounterStore()
+
+const currrentCount = computed(() => {
+  return couter.count
+})
+
+const handleIncrease = () => {
+  couter.increment()
+}
+</script>
 
 <style lang="scss" scoped>
 .appTest {
-	.child {
-		color: blue;
-	}
+  .child {
+    color: blue;
+  }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+//Pinia
+import { createPinia } from 'pinia'
+
 //taildwindcss
 import 'tailwindcss/tailwind.css'
 // reset style sheet
@@ -12,5 +15,7 @@ import '@/styles/reset.css'
 import App from './App.vue'
 
 const app = createApp(App)
+const pinia = createPinia()
 app.use(ElementPlus)
+app.use(pinia)
 app.mount('#app')
