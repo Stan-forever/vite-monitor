db94babf2e31ed03d2d3818e97c62f04f7824c86
xujunsheng <1725116377@qq.com>
2024-07-09 13:43:05 +0800

    feat:国际化

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <RouterView />
+  <el-config-provider :locale="locale">
+    <RouterView />
+  </el-config-provider>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
+const locale = ref(zhCn)
+</script>
 
 <style lang="scss" scoped></style>
